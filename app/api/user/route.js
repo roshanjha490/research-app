acd43f6d6bf7fea0337f536ec81cdd3e9bcc9351
roshanjha490 <kumarroshanjha786@gmail.com>
2024-03-07 13:09:53 +0530

    07 March 2024

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -1,20 +1,29 @@
 import { NextResponse } from "next/server";
-import { get_table_data_by_array } from "@/lib/db";
+import { get_table_data_by_array, insert_data_in_table } from "@/lib/db";
 
 export async function GET(request) {
+    // let sq1 = {
+    //     table_name: 'users',
+    //     where_array: {
+    //         id: 1
+    //     },
+    //     order_by: 'id'
+    // }
 
-    let sql = {
-        table_name: 'users',
-        where_array: {
-            id: 1
-        },
-        order_by: 'id'
-    }
+    // let sql2 = {
+    //     table_name: 'users',
+    //     data: {
+    //         name: 'Pankaj Jha',
+    //         email: 'abc@gmail.com',
+    //         password: '131242412'
+    //     }
+    // }
 
-    return NextResponse.json({
-        success: true,
-        result: await get_table_data_by_array(sql)
-    })
+    // return NextResponse.json({
+        // success: true,
+        // result: await get_table_data_by_array(sql)
+    //     result: await insert_data_in_table(sql2)
+    // })
 }
 
 
